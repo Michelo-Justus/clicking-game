81ee31ea32777f2a73cf076d96917da48d8a5de1
Michelo-Justus <justusmichelo@gmail.com>
2022-08-25 23:00:03 +0200

    25/08/22

--- js/index.js
+++ js/index.js
@@ -1,14 +1,22 @@
 let counter=0;
 
+//counter
 function count(){
+    
     document.querySelector('button').innerHTML="Click Again";
     document.querySelector('h1').innerHTML=counter++;
-    if (counter>0){
-        document.querySelector('button').innerHTML="Click Faster!";
-    }
-}
+    timer();
+}   
+
+//stop button
 function disable(){
     document.getElementById('btn').disabled = true;
     document.querySelector('button').innerHTML="Game Over";
     btn.style.backgroundColor = 'grey';
+}
+
+function timer(){
+    if (counter>0){
+        document.querySelector('button').innerHTML="Click Faster!";
+    }
 }
